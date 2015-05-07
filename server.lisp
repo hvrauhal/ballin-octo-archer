25a422240d5ed569f70c6adef7d9c46884f3d1c4
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2015-05-07 15:12:54 +0300

    extract things and use cons cells

--- server.lisp
+++ server.lisp
@@ -5,8 +5,9 @@
 (defun real-udp-handler (buffer)
   (declare (type (simple-array (unsigned-byte 8) *) buffer))
   (format t "S: Got a packet: ~A~%S: From ~A:~A~%" (json-octets-to-alist buffer) usocket:*remote-host* usocket:*remote-port*)
+  (format t "S: Local addresses are: ~A:~A~%" *server-host* *server-port*)
   (if *should-break* (error "Done..."))
-  (alist-to-json-octets '((type . pong) (connection-id . "abcdef"))))
+  (alist-to-json-octets (list (cons 'type 'connect-ok) '(connection-id . "abcdef") (cons 'address *server-host*) (cons 'port *server-port*))))
 
 (defun udp-handler-handle (buffer) 
   (real-udp-handler buffer))
@@ -23,12 +24,20 @@
        (sample-as-alist (json-octets-to-alist sample-octets)))
   (cdr (assoc :type sample-as-alist)))
 
+(defparameter *server-port* nil)
+(defparameter *server-host* )
+
+
 (defun start-server () 
   (sb-thread:make-thread 
    #'(lambda (standard-output)
-       (let ((*standard-output* standard-output)
-             (port 12321))
-         (format t "S: Starting the server at port ~A~%" port)
-         (usocket:socket-server usocket:*wildcard-host* port #'udp-handler-handle nil :protocol :datagram)
-         (format t "S: Server done.")))
+       (progn
+
+         (let* ((*standard-output* standard-output)
+                (port 12321)
+                (*server-port* port)
+                (*server-host* "127.0.0.1"))
+           (format t "S: Starting the server at port ~A~%" port)
+           (usocket:socket-server *server-host* port #'udp-handler-handle nil :protocol :datagram)
+           (format t "S: Server done."))))
    :arguments (list *standard-output*)))
